f6bd851050790952529be0d1eebb20f73a89bf6f
= <=>
2021-12-04 18:47:00 -0700

    New files added

---
+++ lsi.py
@@ -0,0 +1,228 @@
+r"""
+Similarity Queries
+==================
+
+Demonstrates querying a corpus for similar documents.
+"""
+
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+###############################################################################
+#
+# Creating the Corpus
+# -------------------
+#
+# First, we need to create a corpus to work with.
+# This step is the same as in the previous tutorial;
+# if you completed it, feel free to skip to the next section.
+
+from collections import defaultdict
+from gensim import corpora
+import json
+# read corpus file
+#  tagged map
+tagged_corpus = open('input_tagged_corpus.json', 'r')
+tagged_corpus = json.load(tagged_corpus)
+count = 1
+for seminars in tagged_corpus: 
+    seminars["id"] = count
+    count += 1
+
+with open('input_tagged_corpus_id.json', 'w') as outfile:
+    json.dump(tagged_corpus, outfile)
+
+#  untagged map
+untagged_corpus = open('input_untagged_corpus.json', 'r')
+untagged_corpus = json.load(untagged_corpus)
+
+for seminars in untagged_corpus: 
+    seminars["id"] = count
+    count += 1
+
+with open('input_untagged_corpus_id.json', 'w') as outfile:
+    json.dump(untagged_corpus, outfile)
+
+# remove common words and tokenize
+from gensim.parsing.preprocessing import remove_stopwords
+texts = [remove_stopwords(seminars["description"].lower()).split() for seminars in tagged_corpus]
+
+
+
+topic_map = {}
+#  map where keys represent the tags in each document and its corresponding document
+#  example:
+#  {'engineering': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '13', '14', '15', '17', '19'], 'teaching': ['10', '16', '18', '20']}
+for seminars in tagged_corpus: 
+    if seminars["tag"].lower() not in topic_map:
+        topic_map[seminars["tag"].lower()] = [seminars["id"]]
+    else:
+        topic_map[seminars["tag"].lower()].append(seminars["id"])
+
+# remove words that appear only once
+# frequency = defaultdict(int)
+# for text in texts:
+#     for token in text:
+#         frequency[token] += 1
+
+# texts = [
+#     [token for token in text if frequency[token] > 0]
+#     for text in texts
+# ]
+
+dictionary = corpora.Dictionary(texts)
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+###############################################################################
+# Similarity interface
+# --------------------
+#
+# In the previous tutorials on
+# :ref:`sphx_glr_auto_examples_core_run_corpora_and_vector_spaces.py`
+# and
+# :ref:`sphx_glr_auto_examples_core_run_topics_and_transformations.py`,
+# we covered what it means to create a corpus in the Vector Space Model and how
+# to transform it between different vector spaces. A common reason for such a
+# charade is that we want to determine **similarity between pairs of
+# documents**, or the **similarity between a specific document and a set of
+# other documents** (such as a user query vs. indexed documents).
+#
+# To show how this can be done in gensim, let us consider the same corpus as in the
+# previous examples (which really originally comes from Deerwester et al.'s
+# `"Indexing by Latent Semantic Analysis" <http://www.cs.bham.ac.uk/~pxt/IDA/lsa_ind.pdf>`_
+# seminal 1990 article).
+# To follow Deerwester's example, we first use this tiny corpus to define a 2-dimensional
+# LSI space:
+
+from gensim import models
+lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=100)
+
+###############################################################################
+# For the purposes of this tutorial, there are only two things you need to know about LSI.
+# First, it's just another transformation: it transforms vectors from one space to another.
+# Second, the benefit of LSI is that enables identifying patterns and relationships between terms (in our case, words in a document) and topics.
+# Our LSI space is two-dimensional (`num_topics = 2`) so there are two topics, but this is arbitrary.
+# If you're interested, you can read more about LSI here: `Latent Semantic Indexing <https://en.wikipedia.org/wiki/Latent_semantic_indexing>`_:
+#
+# Now suppose a user typed in the query `"Human computer interaction"`. We would
+# like to sort our nine corpus documents in decreasing order of relevance to this query.
+# Unlike modern search engines, here we only concentrate on a single aspect of possible
+# similarities---on apparent semantic relatedness of their texts (words). No hyperlinks,
+# no random-walk static ranks, just a semantic extension over the boolean keyword match:
+from gensim import similarities
+# index = similarities.MatrixSimilarity(lsi[corpus])
+f = open("output.txt", "a")
+for seminars in untagged_corpus:
+    doc = seminars["description"]
+    vec_bow = dictionary.doc2bow(doc.lower().split())
+    vec_lsi = lsi[vec_bow]  # convert the query to LSI space
+    index = similarities.MatrixSimilarity(lsi[corpus])
+    index.save('/tmp/deerwester.index')
+    index = similarities.MatrixSimilarity.load('/tmp/deerwester.index')
+    sims = index[vec_lsi]
+    f.write(str(seminars["id"]) +',  ')
+    sims = sorted(enumerate(sims), key=lambda item: -item[1])
+    count = 0
+    for doc_position, doc_score in sims:
+        if count == 0:
+            f.write('(' + str(doc_score) + ', ' + str(tagged_corpus[doc_position]["tag"]) + ', ' + str(seminars["tag"]) + ')')
+            f.write("\n")
+            topic_map[str(tagged_corpus[doc_position]["tag"]).lower()].append(seminars["id"])
+        count += 1
+f.write('\n' + str(topic_map))
+
+###############################################################################
+# In addition, we will be considering `cosine similarity <http://en.wikipedia.org/wiki/Cosine_similarity>`_
+# to determine the similarity of two vectors. Cosine similarity is a standard measure
+# in Vector Space Modeling, but wherever the vectors represent probability distributions,
+# `different similarity measures <http://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence#Symmetrised_divergence>`_
+# may be more appropriate.
+#
+# Initializing query structures
+# ++++++++++++++++++++++++++++++++
+#
+# To prepare for similarity queries, we need to enter all documents which we want
+# to compare against subsequent queries. In our case, they are the same nine documents
+# used for training LSI, converted to 2-D LSA space. But that's only incidental, we
+# might also be indexing a different corpus altogether.
+
+
+# index = similarities.MatrixSimilarity(lsi[corpus])  # transform corpus to LSI space and index it
+
+###############################################################################
+# .. warning::
+#   The class :class:`similarities.MatrixSimilarity` is only appropriate when the whole
+#   set of vectors fits into memory. For example, a corpus of one million documents
+#   would require 2GB of RAM in a 256-dimensional LSI space, when used with this class.
+#
+#   Without 2GB of free RAM, you would need to use the :class:`similarities.Similarity` class.
+#   This class operates in fixed memory, by splitting the index across multiple files on disk, called shards.
+#   It uses :class:`similarities.MatrixSimilarity` and :class:`similarities.SparseMatrixSimilarity` internally,
+#   so it is still fast, although slightly more complex.
+#
+# Index persistency is handled via the standard :func:`save` and :func:`load` functions:
+
+
+
+###############################################################################
+# This is true for all similarity indexing classes (:class:`similarities.Similarity`,
+# :class:`similarities.MatrixSimilarity` and :class:`similarities.SparseMatrixSimilarity`).
+# Also in the following, `index` can be an object of any of these. When in doubt,
+# use :class:`similarities.Similarity`, as it is the most scalable version, and it also
+# supports adding more documents to the index later.
+#
+# Performing queries
+# ++++++++++++++++++
+#
+# To obtain similarities of our query document against the nine indexed documents:
+
+# sims = index[vec_lsi]  # perform a similarity query against the corpus
+# print(list(enumerate(sims)))  # print (document_number, document_similarity) 2-tuples
+
+###############################################################################
+# Cosine measure returns similarities in the range `<-1, 1>` (the greater, the more similar),
+# so that the first document has a score of 0.99809301 etc.
+#
+# With some standard Python magic we sort these similarities into descending
+# order, and obtain the final answer to the query `"Human computer interaction"`:
+
+# sims = sorted(enumerate(sims), key=lambda item: -item[1])
+# for doc_position, doc_score in sims:
+#     print(doc_score, documents[doc_position])
+
+###############################################################################
+# The thing to note here is that documents no. 2 (``"The EPS user interface management system"``)
+# and 4 (``"Relation of user perceived response time to error measurement"``) would never be returned by
+# a standard boolean fulltext search, because they do not share any common words with ``"Human
+# computer interaction"``. However, after applying LSI, we can observe that both of
+# them received quite high similarity scores (no. 2 is actually the most similar!),
+# which corresponds better to our intuition of
+# them sharing a "computer-human" related topic with the query. In fact, this semantic
+# generalization is the reason why we apply transformations and do topic modelling
+# in the first place.
+#
+# Where next?
+# ------------
+#
+# Congratulations, you have finished the tutorials -- now you know how gensim works :-)
+# To delve into more details, you can browse through the :ref:`apiref`,
+# see the :ref:`wiki` or perhaps check out :ref:`distributed` in `gensim`.
+#
+# Gensim is a fairly mature package that has been used successfully by many individuals and companies, both for rapid prototyping and in production.
+# That doesn't mean it's perfect though:
+#
+# * there are parts that could be implemented more efficiently (in C, for example), or make better use of parallelism (multiple machines cores)
+# * new algorithms are published all the time; help gensim keep up by `discussing them <http://groups.google.com/group/gensim>`_ and `contributing code <https://github.com/piskvorky/gensim/wiki/Developer-page>`_
+# * your **feedback is most welcome** and appreciated (and it's not just the code!):
+#   `bug reports <https://github.com/piskvorky/gensim/issues>`_ or
+#   `user stories and general questions <http://groups.google.com/group/gensim/topics>`_.
+#
+# Gensim has no ambition to become an all-encompassing framework, across all NLP (or even Machine Learning) subfields.
+# Its mission is to help NLP practitioners try out popular topic modelling algorithms
+# on large datasets easily, and to facilitate prototyping of new algorithms for researchers.
+
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# img = mpimg.imread('run_similarity_queries.png')
+# imgplot = plt.imshow(img)
+# _ = plt.axis('off')
---
+++ tf-idf.py
@@ -0,0 +1,318 @@
+r"""
+Core Concepts
+=============
+
+This tutorial introduces Documents, Corpora, Vectors and Models: the basic concepts and terms needed to understand and use gensim.
+"""
+
+# mock data format - create corpus map
+# have tagged/training corpus and use untagged corpus as query
+# 
+import json
+# read corpus file
+#  tagged map
+tagged_corpus = open('input_tagged_corpus.json', 'r')
+tagged_corpus = json.load(tagged_corpus)
+count = 1
+for seminars in tagged_corpus: 
+    seminars["id"] = count
+    count += 1
+
+with open('input_tagged_corpus_id.json', 'w') as outfile:
+    json.dump(tagged_corpus, outfile)
+
+#  untagged map
+
+untagged_corpus = open('input_untagged_corpus.json', 'r')
+untagged_corpus = json.load(untagged_corpus)
+for seminars in untagged_corpus: 
+    seminars["id"] = count
+    count += 1
+
+with open('input_untagged_corpus_id.json', 'w') as outfile:
+    json.dump(untagged_corpus, outfile)
+
+
+# create topic map
+
+# remove stoplist
+# remove infrequent words
+# word to int id map using dictionary
+# convert corpus to list of vectors
+# train tfidf model with the bow corpus
+# find similarity between desc
+# pick the document with most similarity and add them to topic map
+#  add tag to corpus
+#  if no sim found, use topic 
+# create speaker map
+
+
+import pprint
+from gensim.parsing.preprocessing import remove_stopwords
+# Create a set of frequent words
+# Lowercase each document, split it by white space and filter out stopwords
+texts = [remove_stopwords(seminars["description"].lower()).split() for seminars in tagged_corpus]
+texts = [[token for token in text if token.isalpha() and len(token) >=3 ] for text in texts]
+
+# print(texts)
+
+# Count word frequencies
+from collections import defaultdict
+frequency = defaultdict(int)
+for text in texts:
+    for token in text:
+        frequency[token] += 1
+
+# Only keep words that appear more than once
+processed_corpus = [[token for token in text if frequency[token] > 3] for text in texts]
+# pprint.pprint(processed_corpus)
+
+topic_map = {}
+#  map where keys represent the tags in each document and its corresponding document
+#  example:
+#  {'engineering': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '13', '14', '15', '17', '19'], 'teaching': ['10', '16', '18', '20']}
+for seminars in tagged_corpus: 
+    if seminars["tag"].lower() not in topic_map:
+        topic_map[seminars["tag"].lower()] = [seminars["id"]]
+    else:
+        topic_map[seminars["tag"].lower()].append(seminars["id"])
+
+###############################################################################
+# Before proceeding, we want to associate each word in the corpus with a unique
+# integer ID. We can do this using the :py:class:`gensim.corpora.Dictionary`
+# class.  This dictionary defines the vocabulary of all words that our
+# processing knows about.
+#
+from gensim import corpora
+
+dictionary = corpora.Dictionary(processed_corpus)
+# print(dictionary)
+
+###############################################################################
+# Because our corpus is small, there are only 12 different tokens in this
+# :py:class:`gensim.corpora.Dictionary`. For larger corpuses, dictionaries that
+# contains hundreds of thousands of tokens are quite common.
+#
+
+###############################################################################
+# .. _core_concepts_vector:
+#
+# Vector
+# ------
+#
+# To infer the latent structure in our corpus we need a way to represent
+# documents that we can manipulate mathematically. One approach is to represent
+# each document as a vector of *features*.
+# For example, a single feature may be thought of as a question-answer pair:
+#
+# 1. How many times does the word *splonge* appear in the document? Zero.
+# 2. How many paragraphs does the document consist of? Two.
+# 3. How many fonts does the document use? Five.
+#
+# The question is usually represented only by its integer id (such as `1`, `2` and `3`).
+# The representation of this document then becomes a series of pairs like ``(1, 0.0), (2, 2.0), (3, 5.0)``.
+# This is known as a *dense vector*, because it contains an explicit answer to each of the above questions.
+#
+# If we know all the questions in advance, we may leave them implicit
+# and simply represent the document as ``(0, 2, 5)``.
+# This sequence of answers is the **vector** for our document (in this case a 3-dimensional dense vector).
+# For practical purposes, only questions to which the answer is (or
+# can be converted to) a *single floating point number* are allowed in Gensim.
+#
+# In practice, vectors often consist of many zero values.
+# To save memory, Gensim omits all vector elements with value 0.0.
+# The above example thus becomes ``(2, 2.0), (3, 5.0)``.
+# This is known as a *sparse vector* or *bag-of-words vector*.
+# The values of all missing features in this sparse representation can be unambiguously resolved to zero, ``0.0``.
+#
+# Assuming the questions are the same, we can compare the vectors of two different documents to each other.
+# For example, assume we are given two vectors ``(0.0, 2.0, 5.0)`` and ``(0.1, 1.9, 4.9)``.
+# Because the vectors are very similar to each other, we can conclude that the documents corresponding to those vectors are similar, too.
+# Of course, the correctness of that conclusion depends on how well we picked the questions in the first place.
+#
+# Another approach to represent a document as a vector is the *bag-of-words
+# model*.
+# Under the bag-of-words model each document is represented by a vector
+# containing the frequency counts of each word in the dictionary.
+# For example, assume we have a dictionary containing the words
+# ``['coffee', 'milk', 'sugar', 'spoon']``.
+# A document consisting of the string ``"coffee milk coffee"`` would then
+# be represented by the vector ``[2, 1, 0, 0]`` where the entries of the vector
+# are (in order) the occurrences of "coffee", "milk", "sugar" and "spoon" in
+# the document. The length of the vector is the number of entries in the
+# dictionary. One of the main properties of the bag-of-words model is that it
+# completely ignores the order of the tokens in the document that is encoded,
+# which is where the name bag-of-words comes from.
+#
+# Our processed corpus has 12 unique words in it, which means that each
+# document will be represented by a 12-dimensional vector under the
+# bag-of-words model. We can use the dictionary to turn tokenized documents
+# into these 12-dimensional vectors. We can see what these IDs correspond to:
+#
+# pprint.pprint(dictionary.token2id)
+
+###############################################################################
+# For example, suppose we wanted to vectorize the phrase "Human computer
+# interaction" (note that this phrase was not in our original corpus). We can
+# create the bag-of-word representation for a document using the ``doc2bow``
+# method of the dictionary, which returns a sparse representation of the word
+# counts:
+#
+
+# new_doc = "Human computer interaction"
+# new_vec = dictionary.doc2bow(new_doc.lower().split())
+# print(new_vec)
+
+###############################################################################
+# The first entry in each tuple corresponds to the ID of the token in the
+# dictionary, the second corresponds to the count of this token.
+#
+# Note that "interaction" did not occur in the original corpus and so it was
+# not included in the vectorization. Also note that this vector only contains
+# entries for words that actually appeared in the document. Because any given
+# document will only contain a few words out of the many words in the
+# dictionary, words that do not appear in the vectorization are represented as
+# implicitly zero as a space saving measure.
+#
+# We can convert our entire original corpus to a list of vectors:
+#
+bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
+# pprint.pprint(bow_corpus)
+
+###############################################################################
+# Note that while this list lives entirely in memory, in most applications you
+# will want a more scalable solution. Luckily, ``gensim`` allows you to use any
+# iterator that returns a single document vector at a time. See the
+# documentation for more details.
+#
+# .. Important::
+#   The distinction between a document and a vector is that the former is text,
+#   and the latter is a mathematically convenient representation of the text.
+#   Sometimes, people will use the terms interchangeably: for example, given
+#   some arbitrary document ``D``, instead of saying "the vector that
+#   corresponds to document ``D``", they will just say "the vector ``D``" or
+#   the "document ``D``".  This achieves brevity at the cost of ambiguity.
+#
+#   As long as you remember that documents exist in document space, and that
+#   vectors exist in vector space, the above ambiguity is acceptable.
+#
+# .. Important::
+#   Depending on how the representation was obtained, two different documents
+#   may have the same vector representations.
+#
+# .. _core_concepts_model:
+#
+# Model
+# -----
+#
+# Now that we have vectorized our corpus we can begin to transform it using
+# *models*. We use model as an abstract term referring to a *transformation* from
+# one document representation to another. In ``gensim`` documents are
+# represented as vectors so a model can be thought of as a transformation
+# between two vector spaces. The model learns the details of this
+# transformation during training, when it reads the training
+# :ref:`core_concepts_corpus`.
+#
+# One simple example of a model is `tf-idf
+# <https://en.wikipedia.org/wiki/Tf%E2%80%93idf>`_.  The tf-idf model
+# transforms vectors from the bag-of-words representation to a vector space
+# where the frequency counts are weighted according to the relative rarity of
+# each word in the corpus.
+#
+# Here's a simple example. Let's initialize the tf-idf model, training it on
+# our corpus and transforming the string "system minors":
+#
+
+from gensim import models
+
+# train the model
+tfidf = models.TfidfModel(bow_corpus)
+
+# transform the "system minors" string
+# words = "system minors".lower().split()
+# print(tfidf[dictionary.doc2bow(words)])
+
+###############################################################################
+# The ``tfidf`` model again returns a list of tuples, where the first entry is
+# the token ID and the second entry is the tf-idf weighting. Note that the ID
+# corresponding to "system" (which occurred 4 times in the original corpus) has
+# been weighted lower than the ID corresponding to "minors" (which only
+# occurred twice).
+#
+# You can save trained models to disk and later load them back, either to
+# continue training on new training documents or to transform new documents.
+#
+# ``gensim`` offers a number of different models/transformations.
+# For more, see :ref:`sphx_glr_auto_examples_core_run_topics_and_transformations.py`.
+#
+# Once you've created the model, you can do all sorts of cool stuff with it.
+# For example, to transform the whole corpus via TfIdf and index it, in
+# preparation for similarity queries:
+#
+from gensim import similarities
+
+
+###############################################################################
+# and to query the similarity of our query document ``query_document`` against every document in the corpus:
+# run a loop for seminars and find similar seminars add them to topic map
+
+# query_document ="The Center for Cultural Analysis will host a lecture by Richard Maddox entitled 'The Best of Possible Islands: Seville, Expo '92, and the Politics of Culture in the 'New Spain', at 3:30 p.m., Friday, March 17, in Baker Hall 235A.All are welcome"
+# # print(query_document)
+# query_bow = dictionary.doc2bow(query_document.lower().split())
+# sims = index[tfidf[query_bow]]
+# print(list(enumerate(sims)))
+
+
+# index = similarities.MatrixSimilarity(lsi[corpus])
+f = open("output_tf_idf.txt", "a")
+for seminars in untagged_corpus:
+    doc = seminars["description"]
+    vec_bow = dictionary.doc2bow(doc.lower().split())
+    index = similarities.SparseMatrixSimilarity(tfidf[bow_corpus], num_features=300)
+    sims = index[tfidf[vec_bow]]
+    f.write(str(seminars["id"]) +',  ')
+    sims = sorted(enumerate(sims), key=lambda item: -item[1])
+    count = 0
+    for doc_position, doc_score in sims:
+        if count == 0:
+            f.write('(' + str(doc_score) + ', ' + str(tagged_corpus[doc_position]["tag"]) + ', ' + str(seminars["tag"]) + ')')
+            f.write("\n")
+            topic_map[str(tagged_corpus[doc_position]["tag"]).lower()].append(seminars["id"])
+        count += 1
+f.write('\n' + str(topic_map))
+
+###############################################################################
+# How to read this output?
+# Document 3 has a similarity score of 0.718=72%, document 2 has a similarity score of 42% etc.
+# We can make this slightly more readable by sorting:
+
+# for document_number, score in sorted(enumerate(sims), key=lambda x: x[1], reverse=True):
+#     print(document_number, score)
+
+###############################################################################
+# Summary
+# -------
+#
+# The core concepts of ``gensim`` are:
+#
+# 1. :ref:`core_concepts_document`: some text.
+# 2. :ref:`core_concepts_corpus`: a collection of documents.
+# 3. :ref:`core_concepts_vector`: a mathematically convenient representation of a document.
+# 4. :ref:`core_concepts_model`: an algorithm for transforming vectors from one representation to another.
+#
+# We saw these concepts in action.
+# First, we started with a corpus of documents.
+# Next, we transformed these documents to a vector space representation.
+# After that, we created a model that transformed our original vector representation to TfIdf.
+# Finally, we used our model to calculate the similarity between some query document and all documents in the corpus.
+#
+# What Next?
+# ----------
+#
+# There's still much more to learn about :ref:`sphx_glr_auto_examples_core_run_corpora_and_vector_spaces.py`.
+
+# # import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# img = mpimg.imread('run_core_concepts.png')
+# imgplot = plt.imshow(img)
+# _ = plt.axis('off')
